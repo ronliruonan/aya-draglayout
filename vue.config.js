03e615d6ba6f19d5d797ebde437d0d9821087494
Ruonan Li <liruonan@hotmail.com>
2018-10-24 17:48:04 +0800

    webpack

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+module.exports = {
+  baseUrl: ""
+  // configureWebpack: config => {
+  //   if (process.env.NODE_ENV === "production") {
+  //     // 为生产环境修改配置
+  //     config.
+  //   } else {
+  //     // 为开发环境修改配置
+  //   }
+  // }
+};
